4977fe829b2266206014cf78682215a5496f1cf7
AHA-EchoNet <aha.echonet@gmail.com>
2025-12-02 22:08:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js
 // Enkel service worker for AHA Chat – offline + cache av app-shell
 
-const CACHE_NAME = "aha-chat-v1.0.0.102";
+const CACHE_NAME = "aha-chat-v1.0.0.103";
 
 // Justér stier hvis nettstedet ligger i en undermappe
 const ASSETS = [
